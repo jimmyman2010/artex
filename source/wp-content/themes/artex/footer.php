2a2bccb51be76950a249853772b1b5994fe13a11
jimmyman2010 <tranduyminhman@gmail.com>
2015-09-03 18:36:48 +0700

    reinit artex theme

--- source/wp-content/themes/artex/footer.php
+++ source/wp-content/themes/artex/footer.php
@@ -3,12 +3,9 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 				<div class="container">
-					<!-- copyright -->
-					<p class="copyright">
-						&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-						<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-					</p>
-					<!-- /copyright -->
+					<!-- back to top -->
+                    <a class="back-to-top" href="javascript:;"></a>
+					<!-- /back to top -->
 				</div>
 			</footer>
 			<!-- /footer -->
