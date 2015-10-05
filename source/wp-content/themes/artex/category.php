6ac7f0cf1efd336af4c68639a51c885f486a55c4
Jimmy Man <tranduyminhman@gmail.com>
2015-10-05 18:26:33 +0700

    update

--- source/wp-content/themes/artex/category.php
+++ source/wp-content/themes/artex/category.php
@@ -1,17 +1,39 @@
 <?php get_header(); ?>
 
-	<!-- section -->
-	<section>
+<?php get_sidebar('left'); ?>
 
-		<h1><?php _e( 'Categories for ', 'html5blank' ); single_cat_title(); ?></h1>
+<div class="col-sm-6 col-sm-offset-1">
 
+	<?php
+	$quotesList = get_posts([
+		'posts_per_page' => 10000,
+		'post_type' => 'quotes'
+	]);
+
+	$index = array_rand($quotesList, 1);
+
+	$custom_fields = get_post_custom($quotesList[$index]->ID);
+
+	wp_reset_postdata();
+	?>
+
+	<div class="random-quotes">
+		<h3><?= $custom_fields['quotes'][0] ?></h3>
+		<?php if(!empty($custom_fields['source'][0])) { ?>
+		<a href="<?= $custom_fields['source'][0] ?>">
+			<?php } ?>
+			<p><?= $custom_fields['author'][0] ?></p>
+			<?php if(!empty($custom_fields['source'][0])) { ?>
+		</a>
+	<?php } ?>
+		<a href="javascript:void(0);" class="refresh">refresh</a>
+		<a href="<?= get_permalink($quotesList[$index]->ID) ?>" class="open-link">detail</a>
+	</div>
+	<div class="blog-list row">
 		<?php get_template_part('loop'); ?>
-
 		<?php get_template_part('pagination'); ?>
-
-	</section>
-	<!-- /section -->
-
-<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php get_sidebar('right'); ?>
 
 <?php get_footer(); ?>
