4fc0b263c6c7846607f3f32bff634feb91bea77c
Jimmy Man <tranduyminhman@gmail.com>
2015-10-07 10:44:21 +0700

    update menu

--- source/wp-content/themes/artex/js/scripts.js
+++ source/wp-content/themes/artex/js/scripts.js
@@ -15,7 +15,41 @@
 
     $('.close-sidebar').on('click', function(){
         var sidebar = $(this).parent();
-        sidebar.css({'z-index': '-1'});
+        closeSidebar(sidebar);
+    });
+
+    $('.show-menu').on('click', function(){
+        var that = $(this);
+        var sidebar = $(that.data('target'));
+
+        if(sidebar.hasClass('open')) {
+            closeSidebar(sidebar);
+        }
+        else {
+            openSidebar(that, sidebar);
+        }
+    });
+
+    function openSidebar(button, sidebar) {
+        sidebar.addClass('open').show();
+        var widthWindow = $(window).width();
+        $('body').width(widthWindow).css('overflow-x', 'hidden');
+        var path = 260;
+        if(!button.hasClass('left-menu')) {
+            path = -260;
+            $('.left-menu').fadeOut();
+        }
+        else {
+            $('.right-menu').fadeOut();
+        }
+        $('.wrapper').css({'transform': 'translate('+path+'px)', 'transition-duration': '500ms'});
+        setTimeout(function(){
+            sidebar.css({'z-index': '1'});
+        }, 500);
+    }
+
+    function closeSidebar(sidebar) {
+        sidebar.removeClass('open').css({'z-index': '-1'});
         $('.wrapper').css({'transform': 'translate(0)'});
         setTimeout(function(){
             $('.wrapper').removeAttr('style');
@@ -27,27 +61,7 @@
 
         $('.left-menu').fadeIn();
         $('.right-menu').fadeIn();
-    });
-
-    $('.show-menu').on('click', function(){
-        var that = $(this);
-        var widthWindow = $(window).width();
-
-        $(that.data('target')).show();
-        $('body').width(widthWindow).css('overflow-x', 'hidden');
-        var path = 260;
-        if(!that.hasClass('left-menu')) {
-            path = -260;
-            $('.left-menu').fadeOut();
-        }
-        else {
-            $('.right-menu').fadeOut();
-        }
-        $('.wrapper').css({'transform': 'translate('+path+'px)', 'transition-duration': '500ms'});
-        setTimeout(function(){
-            $(that.data('target')).css({'z-index': '1'});
-        }, 500);
-    });
+    }
 
     $('.back-to-top').on('click', function(){
         $("html, body").animate({scrollTop: 0}, 500);
